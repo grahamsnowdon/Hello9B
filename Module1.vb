372ebfceceeae025ca2e40944fa5c2c9763dab6f
GrahamSnowdon <graham@snowdon.me>
2022-12-14 11:52:37 +0000

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+    'This is a program to demonstrate the power of github to my Y9's
+    Sub Main()
+        Console.WriteLine("Hello 9B, you lovely bunch of cauflowers.")
+        Console.WriteLine("Bonjour 9B, mon petite chaufluer. ")
+    End Sub
+
+End Module
